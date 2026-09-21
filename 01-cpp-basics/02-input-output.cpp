16b37fb60451c57a6475ce01232ad7cc69954a27
salonshrestha0 <273480929+salonshrestha0@users.noreply.github.com>
2026-09-21 20:48:26 +0545

    Add basic input/output example in C++
    This program prompts the user for their name and greets them.

---
+++ 01-cpp-basics/02-input-output.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+   string name;
+   cout << "Enter your name: ";
+    cin >> name; 
+  //cin>>  used to get user input 
+    cout << "Hello, " << name << "have a nice day" << endl;
+  //cout <<to print the content user defined or predifined
+
+    return 0;
+}
