324fd62b466b82fc0362b45ce62305c3f4fbba04
salonshrestha0 <273480929+salonshrestha0@users.noreply.github.com>
2026-09-21 20:34:49 +0545

    Implement variable declaration and data types example
    Added a C++ program demonstrating variable declaration and data types.

---
+++ 01-cpp-basics/01-variables.cpp
@@ -0,0 +1,18 @@
+
+//Topic : Variable and Data types in C++
+#include <iostream>
+using namespace std;
+int main() {
+
+//variable must be declared before it is used. 
+//must be unique name and cannot be a keyword.,no number and special symbols in the variable name.
+
+int a = 10;       //store int value
+double d = 3.14;  //store double value
+char c = 'A';     //store char value
+string str = "Salon_shrestha";    //store string value
+bool B = true;    //store boolean value true or false only
+
+
+    return 0;
+}
